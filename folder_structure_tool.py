023a0ac4e3cfc01708dfc37392586418bcc8b963
Tomás Ó Dálaigh <tom1996.18@gmail.com>
2023-08-30 19:35:37 +0100

    tree improvement option

--- folder_structure_tool.py
+++ folder_structure_tool.py
@@ -1,40 +1,46 @@
 import os
 from datetime import datetime
 
-def list_files(startpath, ignore_git, use_icons):
+def list_files(startpath, ignore_git, use_icons, tree_view):
     folder_icon = "\U0001F4C1" if use_icons else "Folder:"
     file_icon = "\U0001F4C4" if use_icons else "File:"
-    structure = ""
     
+    tree_folder = "├── " if tree_view else ""
+    tree_file = "│   " if tree_view else ' ' * 4
+    
+    structure = ""
+
     for root, dirs, files in os.walk(startpath):
         if ignore_git and ".git" in root:
             continue
             
         level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * level
+        indent = ' ' * 4 * level if not tree_view else "│   " * level
         
-        structure += '{}{} {}\n'.format(indent, folder_icon, os.path.basename(root))
-        sub_indent = ' ' * 4 * (level + 1)
+        structure += '{}{}{} {}\n'.format(indent, tree_folder, folder_icon, os.path.basename(root))
+        sub_indent = ' ' * 4 * (level + 1) if not tree_view else "│   " * (level + 1)
         
         for f in files:
-            structure += '{}{} {}\n'.format(sub_indent, file_icon, f)
+            structure += '{}{}{} {}\n'.format(sub_indent, tree_file, file_icon, f)
+            
     return structure
 
 def main():
     folder_path = input("Enter the path of the folder (hit Enter for current folder): ")
     if not folder_path:
         folder_path = os.getcwd()
-    folder_path = os.getcwd()
+
     show_in_terminal = input("Display in terminal? (Y/N): ").strip().lower()
     output_txt = input("Output to .txt file? (Y/N): ").strip().lower()
     include_git = input("Include .git files? (Y/N): ").strip().lower()
     use_dashes = input("Use dashes for indentation? (Y/N): ").strip().lower()
     use_icons = input("Use icons for folders and files? (Y/N): ").strip().lower()
+    tree_view = input("Use tree-like structure? (Y/N): ").strip().lower() == 'y'
     
     ignore_git = include_git != 'y'
     use_icons = use_icons == 'y'
     
-    folder_structure = list_files(folder_path, ignore_git, use_icons)
+    folder_structure = list_files(folder_path, ignore_git, use_icons, tree_view)
     
     if output_txt == 'y':
         txt_filename = "{}_folder_structure.txt".format(os.path.basename(folder_path))
